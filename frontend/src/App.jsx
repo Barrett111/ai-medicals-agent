e475deca67cfa40ccc2d67509b3ae2703f92f7a7
Rushikesh Ranaware <rushikgit@gmail.com>
2026-05-01 00:30:03 +0530

    update backend URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,7 +51,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/chat/", {
+      const res = await fetch("https://ai-medicals-agent-1.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
