c47d884b9030533a328d8face5db68e75e1bfa8c
Rushikesh Ranaware <rushikgit@gmail.com>
2026-05-01 01:07:12 +0530

    fix: add sentence-transformers

--- backend/services/embeddings.py
+++ backend/services/embeddings.py
@@ -1,7 +1,7 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def get_embeddings():
-    print("🔄 Loading embeddings model...")
+    print("🔄 Loading lightweight embeddings...")
     return HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
